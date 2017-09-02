74f248157110ca2e36fb4dfbc5cdb1ed85fc97df
Aditya Kalluraya <aditya@thewebpeople.in>
2017-09-03 00:15:03 +0530

    An ionic android app for News Website

---
+++ www/settings.js
@@ -0,0 +1,18 @@
+//For rate and share app
+var android_packageName = "com.newskadaba.app";
+var apple_id = "";
+//URL site wordpress
+var wordpress_url = "http://newskadaba.com";
+//Posts per page
+var wordpress_per_page = 10;
+//For push notification with OneSignal
+var onesignal_app_id = "7765f63f-b2a8-48ea-b708-0fac5f6f19a4";
+var onesignal_project_number = "387160489339";
+//For RTL Language
+var rtl_language = false;
+//Open link in Detail
+var open_target_blank = true;
+//Date time format
+var date_format = "dd-MM-yyyy";
+//For Google Analytics
+var google_analytics = "UA-77762916-1";
